299301ddca8e006e21d90bbde78144f6e72b699e
Jackson Herman <hermanjacksonw@gmail.com>
2021-09-13 15:24:28 -0500

    Added functionality for skills, abilities, and items

--- bot.py
+++ bot.py
@@ -6,10 +6,13 @@ with open('config.json') as f:
     
 TOKEN = config['BOT_TOKEN']
 puppet_list = {}
+ability_list = {}
+skill_list = {}
+item_list = {}
 
 client = discord.Client()
 
-def set_dict():
+def set_puppets():
     dict_keys = ['name', 'type1', 'type2', 'hp', 'foatk', 'fodef', 'spatk', 'spdef', 'spd', 'bst', 'cost', 'ability1', 'ability2']
     with open('puppets.txt') as fi:
      line = fi.readline()
@@ -21,6 +24,36 @@ def set_dict():
             values[key] = puppet[dict_keys.index(key)]
          puppet_list[puppet[0].lower()] = values
          line = fi.readline()
+
+def set_abilities():
+    with open('AbilityData.csv') as fi:
+        line = fi.readline()
+        while line != '':
+            ability = line.split(',')
+            ability_list[ability[0].lower()] = ability[-1]
+            line = fi.readline()
+
+def set_items():
+    with open('ItemData.csv') as fi:
+        line = fi.readline()
+        while line != '':
+            item = line.split(',')
+            item_list[item[0].lower()] = item[-1]
+            line = fi.readline()
+
+def set_skills():
+    with open('SkillData.csv') as fi:
+        line = fi.readline()
+        while line != '':
+            skill = line.split(',')
+            skill_list[skill[0].lower()] = skill[-1]
+            line = fi.readline()
+
+def set_dict():
+    set_puppets()
+    set_abilities()
+    set_items()
+    set_skills()
 
 
 @client.event
@@ -55,6 +88,16 @@ async def on_message(message):
                     Ability 1: {puppet['ability1']}
                     Ability 2: {puppet['ability2']}
                     """
+            await message.channel.send(msg)
+        elif commands[0].lower() == 'ability':
+            print(commands[1].lower())
+            msg = f"{commands[1]}: {ability_list[commands[1].lower()]}"
+            await message.channel.send(msg)
+        elif commands[0].lower() == 'item':
+            msg = f"{commands[1]}: {item_list[commands[1].lower()]}"
+            await message.channel.send(msg)
+        elif commands[0].lower() == 'skill':
+            msg = f"{commands[1]}: {skill_list[commands[1].lower()]}"
             await message.channel.send(msg)
         elif commands[0].lower() == 'help':
             msg = """
